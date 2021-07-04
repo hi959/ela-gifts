57d3040da8117d2b0aef3626d1ddb6f133f55990
hi959 <jamesshekolov@gmail.com>
2021-07-04 16:34:14 +0300

    asdf

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
+class Categorie(models.Model):
+	name = models.CharField('שם הקטגוריה', max_length=20)
+	
+	def __str__(self):
+		return self.name
+
 class Product(models.Model):
 	name = models.CharField('שם המוצר', max_length=100)
 	price = models.FloatField('מחיר המוצר', validators=[MinValueValidator(5)])
@@ -10,6 +16,7 @@ class Product(models.Model):
 	count = models.PositiveIntegerField('כמות המוצר')
 	tapStatus = models.BooleanField('לעשות מבצע ?', default=False)
 	sale_price = models.FloatField('מחיר המבצע', validators=[MinValueValidator(5)])
+	Category = models.ManyToManyField(Categorie, 'קטגוריות', blank=True, default='uncategorized')
 
 
 	def __str__(self):
