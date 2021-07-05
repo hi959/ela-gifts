73430b4722f784255c53de71ccb2f876e5b01aa2
hi959 <jamesshekolov@gmail.com>
2021-07-05 23:24:45 +0300

    adsf

--- shop/models.py
+++ shop/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
 	name = models.CharField('שם המוצר', max_length=100)
 	price = models.FloatField('מחיר המוצר', validators=[MinValueValidator(5)])
 	description = models.TextField("תיאור המוצר", blank=True)
-	image = models.ImageField('תמונת המוצר',upload_to="product_images/")
+	image = models.ImageField('תמונת המוצר', upload_to="product_images/")
 	count = models.PositiveIntegerField('כמות המוצר')
 	sale = models.BooleanField('לעשות מבצע ?', default=False)
 	sale_price = models.FloatField('מחיר המבצע', validators=[MinValueValidator(5)])
