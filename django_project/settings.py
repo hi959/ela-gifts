f0409f3c09637e2d2e1c8bf4fb1b7da3a8bbf25b
hi959 <jamesshekolov@gmail.com>
2021-07-12 00:35:05 +0300

    asdf

--- django_project/settings.py
+++ django_project/settings.py
@@ -115,13 +115,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = ''
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'shop/static/')
 
 django_heroku.settings(locals())
--- shop/models.py
+++ shop/models.py
@@ -22,3 +22,4 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
+		
