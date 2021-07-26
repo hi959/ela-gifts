cc43e5163186d2d39c7500eb799719b6b0dd5637
hi959 <jamesshekolov@gmail.com>
2021-07-26 14:33:03 +0300

    adsf

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Categorie
 
 def home(request):
 	hot_products = Product.objects.filter(hot_product=True)
 	recomanded_products = Product.objects.filter(recomanded_product=True)
 	products_list = Product.objects.all()
-	return render(request, 'shop/home.html', {'hot_products': hot_products, 'recomanded_products': recomanded_products})
+	categories = Categorie.objects.all()
+	return render(request, 'shop/home.html', {'hot_products': hot_products, 'recomanded_products': recomanded_products, 'categories': categories})
 
 
 def show_product(request, product_id):
