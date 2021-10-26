164d5f98a372eab4633f97455ddde7f000531e5b
hi959 <jamesshokolov@gmail.com>
2021-10-26 22:57:41 +0300

    adsf

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "asdg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.ela.gifts', '139.162.172.111', 'ela.gifts', 'localhost', '127.0.0.1', 'elagifts.herokuapp.com']
 
